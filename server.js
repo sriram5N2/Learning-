2aae1b1c1bd8f82839b68b45179dd888f4dc2e34
sriram5N2 <139240678+sriram5N2@users.noreply.github.com>
2025-07-24 22:36:47 +0530

    Database connection established

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express=require('express');
+const PORT=3000;
+const dotenv=require('dotenv');
+const {MongoClient}=require('mongodb')
+const app=express();
+dotenv.config()
+MongoClient.connect("mongodb+srv://sriramt234:sriram1234@backend.3tlxoc0.mongodb.net/?retryWrites=true&w=majority&appName=BACKEND")
+.then(()=>{
+    console.log('Mongodb connected succesfully')
+}).catch((e)=>{
+  console.log('Database Not Connected')
+})
+
+app.listen(PORT,()=>{
+    console.log('server started running');
+})
